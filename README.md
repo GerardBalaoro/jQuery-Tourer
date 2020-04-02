0e51c69423e9c48dcffd078336d4a8d87dfa4c88
GerardBalaoro <gmbalaoro@outlook.com>
2020-04-02 14:59:13 +0800

    Add demo website

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+    <a href="https://gerardbalaoro.github.io/jQuery-Tourer/"><img src="assets/images/banner.png" alt="jQuery-Tourer"></a>
+    <br>
+    <a href="https://gerardbalaoro.github.io/jQuery-Tourer/"><img src="https://img.shields.io/badge/See%20It%20In%20Action-Click%20Here-purple.svg" alt="Demo"></a>
+    <a href="https://github.com/GerardBalaoro/jQuery-Tourer/releases/latest"><img src="https://img.shields.io/github/release/GerardBalaoro/jQuery-Tourer.svg" alt="VERSION"></a>
+    <a href="https://github.com/GerardBalaoro/jQuery-Tourer/blob/master/LICENSE.md"><img src="https://img.shields.io/github/license/GerardBalaoro/jQuery-Tourer.svg" alt="LICENSE"></a>
+    <img src="https://zenodo.org/badge/doi/10.5281/zenodo.1473134.svg" alt="DOI">
+</p>
+
+### jQuery-Tourer
+
+Official website and documentation for jQuery-Tourer
+
+
+### License
+
+Copyright © 2018 Gerard Balaoro 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+### Credits
+
+* **PrettyDocs Template** - The template used in this project was created by [Xiaoying Riley](https://twitter.com/3rdwave_themes) under the [Creative Commons Attribution 3.0 License (CC BY 3.0)](https://creativecommons.org/licenses/by/3.0/). 
---
+++ assets/js/jquery-tourer.js
@@ -0,0 +1,184 @@
+(function( $ ){
+    $.fn.tour = function () {
+        var wrapper = this
+            steps   = wrapper.children('li'),
+            overlay = $('.tour-overlay'),
+            content = wrapper.find('.tour-content'),
+            trigger = $('.tour-start');
+
+        // Add Tour Wrapper Class
+        this.addClass('tour-wrapper');
+
+        // Create Navigation Elements For Each Step
+        createNavigation(steps, steps.length);
+
+        // Create 'Start-Tour' Event
+        wrapper.on('start-tour', function () {
+            if (!wrapper.hasClass('active')) {
+                wrapper.addClass('active');
+                showStep(steps.eq(0), overlay);
+            }
+        });
+
+        // Create 'Stop-Tour' Event
+        wrapper.on('stop-tour', function () {
+            steps.removeClass('is-selected move-left');
+            wrapper.removeClass('active');
+            overlay.removeClass('is-visible');
+        });
+
+        // Bind Start Event to Trigger Button
+        trigger.on('click', function () {
+            wrapper.trigger('start-tour');
+        });
+
+        // Bind Stop Event to Close Button
+        content.on('click', '.tour-stop, .tour-close', function (event) {
+            wrapper.trigger('stop-tour');
+        });
+
+        // Add Navigation Button Handlers
+        content.on('click', '.tour-prev', function (event) {
+            // Previous, If Available
+            (!$(event.target).hasClass('inactive')) && changeStep(steps, overlay, 'prev');
+        });
+        content.on('click', '.tour-next', function (event) {
+            // Next, If Available
+            (!$(event.target).hasClass('inactive')) && changeStep(steps, overlay, 'next');
+        });
+        
+
+        // Add Swipe Navigation For Mobile Devices
+        content.on('swiperight', function (event) {
+            // Previous, If Available
+            if (!$(wrapper).find('.tour-prev').hasClass('inactive')) changeStep(steps, overlay, 'prev');
+        });
+        content.on('swipeleft', function (event) {
+            // Next, If Available
+            if (!$(wrapper).find('.tour-next').hasClass('inactive')) changeStep(steps, overlay, 'next');
+        });
+
+        // Add Keyboard Navigation
+        $(document).keyup(function (event) {
+            if (event.which == '37' && !steps.filter('.is-selected').find('.tour-prev').hasClass('inactive')) {
+                changeStep(steps, overlay, 'prev');
+            } else if (event.which == '39' && !steps.filter('.is-selected').find('.tour-next').hasClass('inactive')) {
+                changeStep(steps, overlay, 'next');
+            } else if (event.which == '27') {
+                wrapper.trigger('stop-tour');
+            }
+        });
+
+        // Realign Tour Layer On Window Resize
+        $(window).on('resize', function() {
+            if (wrapper.hasClass('active')) {
+                alignStep($('li.tour-step.is-selected'));
+            }
+        });
+
+        return this;
+    }
+
+    function createNavigation(steps, n) {
+        var tourNavigationHtml = '<div class="tour-nav-container">' +
+        '<span class="tour-step-count"><b class="tour-current-step">1</b>/' + n + '</span>' +
+        '<ul class="tour-nav">' +
+            '<li><a href="javascript:void(0)" class="tour-prev">&#171; Previous</a></li>' +
+            '<li><a href="javascript:void(0)" class="tour-next">Next &#187;</a></li>' +
+        '</ul></div><a href="javascript:void(0)" class="tour-close">Close</a>';
+
+        steps.each(function (index) {
+            var step = $(this),
+                stepNumber = index + 1,
+                nextClass = (stepNumber < n) ? '' : 'inactive',
+                prevClass = (stepNumber == 1) ? 'inactive' : '';
+            var nav = $(tourNavigationHtml).find('.tour-next').addClass(nextClass).end().find('.tour-prev').addClass(prevClass).end().find('.tour-current-step').html(stepNumber).end().appendTo(step.children('.tour-content'));
+        });
+    }
+
+    function showStep(step, layer) {
+        // Make Step Unbound if Target Element does not exist
+        if ($(step.attr('target')).length == 0) step.addClass('unbound');
+
+        // Make Wrapper and Overlay Unbound if Step is Unbound
+        if (step.hasClass('unbound')) {
+            step.parent('.tour-wrapper.active').addClass('unbound');
+            $('.tour-overlay').addClass('unbound');
+        } else {
+            step.parent('.tour-wrapper.active').removeClass('unbound');
+            $('.tour-overlay').removeClass('unbound');
+        }      
+        
+        // Scroll to Step, Align to Target, Show Layer
+        smoothScroll(step, function() {
+            alignStep(step);
+            showLayer(layer);
+            step.addClass('is-selected').removeClass('move-left'); 
+        })             
+    }
+
+    function alignStep(step) {        
+        if (isBound(step)) {    
+            var target = $(step.attr('target'));
+            var content = step.children('.tour-content');
+            var left = 0.5;
+            var top = 0.5;
+            if (target.length > 0) {
+                if (content.hasClass('right')) {
+                    top = ((target.offset().top + (target.height() / 2 )) / window.innerHeight);
+                    left = ((target.offset().left + target.width()) / window.innerWidth);
+                } else if (content.hasClass('left')) {
+                    top = ((target.offset().top + (target.height() / 2 )) / window.innerHeight);
+                    left = (target.offset().left / window.innerWidth);
+                } else if (content.hasClass('top')) {
+                    top = (target.offset().top / window.innerHeight);
+                    left = ((target.offset().left + (target.width() / 2)) / window.innerWidth);
+                } else if (content.hasClass('bottom')) {
+                    top = ((target.offset().top + target.height()) / window.innerHeight);
+                    left = ((target.offset().left + (target.width() / 2)) / window.innerWidth);
+                }
+            }
+            step.css('top', (top * 100) + '%');
+            step.css('left', (left * 100) + '%');
+        } else {
+            step.removeAttr('style');
+        }
+    }
+
+    function smoothScroll(element, callback = null) {
+        if (isBound(element)) {  
+            // Wait for the Animation to Finish Before Executing Callback
+            $('body,html').animate({
+                'scrollTop': element.offset().top - (($(window).height() - element.height()) / 2 )
+            }, 100).promise().then(callback);
+        } else {
+            callback();
+        }
+    }
+
+    function isBound(step) {
+        // Check if the Element is Unbound of the Viewport is Mobile
+        return window.innerWidth >= 1100 && !step.hasClass('unbound');
+    }
+
+    function showLayer(layer) {
+        layer.addClass('is-visible').on('webkitAnimationEnd oanimationend msAnimationEnd animationend', function () {
+            layer.removeClass('is-visible');
+        });
+    }
+
+    function changeStep(steps, layer, bool) {
+        var visibleStep = steps.filter('.is-selected');
+        var delay = 0;
+        visibleStep.removeClass('is-selected');
+
+        (bool == 'next') && visibleStep.addClass('move-left');
+
+        setTimeout(function () {
+            (bool == 'next') ?
+            showStep(visibleStep.next(), layer): showStep(visibleStep.prev(), layer);
+        }, delay);
+    }
+
+    $('.tour-wrapper').tour();
+})( jQuery );
