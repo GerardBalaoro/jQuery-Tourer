d513277b3f5f5bec921f5e3309c7f100b125dc6c
Gerard Balaoro <gmbalaoro@outlook.com>
2020-04-02 23:57:34 +0800

    Replace cssnano with csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,49 @@
 const { src, dest, parallel, watch } = require('gulp')
 const sass = require('gulp-sass')
-const cssnano = require('gulp-cssnano')
+const csso = require('gulp-csso')
 const babel = require('gulp-babel')
 const rename = require('gulp-rename')
 const minify = require('gulp-minify')
 
 function js() {
-	return src('src/js/script.js')
-		.pipe(
-			babel({
-				presets: ['@babel/env'],
-			})
-		)
-		.pipe(rename({ basename: 'jquery-tourer' }))
-		.pipe(
-			minify({
-				ext: {
-					min: '.min.js',
-				},
-				preserveComments: 'some'
-			})
-		)
-		.pipe(dest('dist/'))
+    return src('src/js/script.js')
+        .pipe(
+            babel({
+                presets: ['@babel/env'],
+            })
+        )
+        .pipe(rename({ basename: 'jquery-tourer' }))
+        .pipe(
+            minify({
+                ext: {
+                    min: '.min.js',
+                },
+                preserveComments: 'some'
+            })
+        )
+        .pipe(dest('dist/'))
 }
 
 function css() {
-	return src('src/sass/style.scss')
-		.pipe(
-			sass({
-				includePaths: ['node_modules'],
-				sourceMap: true,
-			})
-		)
-		.pipe(rename({ basename: 'jquery-tourer' }))
-		.pipe(dest('dist/'))
-		.pipe(cssnano({
-			discardComments: true
-		}))
-		.pipe(rename({ extname: '.min.css' }))
-		.pipe(dest('dist/'))
+    return src('src/sass/style.scss')
+        .pipe(
+            sass({
+                includePaths: ['node_modules'],
+                sourceMap: true,
+            })
+        )
+        .pipe(rename({ basename: 'jquery-tourer' }))
+        .pipe(dest('dist/'))
+        .pipe(csso({
+            comments: 'exclamation'
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(dest('dist/'))
 }
 
 exports.js = js
 exports.css = css
 exports.default = parallel(exports.js, exports.css)
 exports.watch = function() {
-	watch('src/**/*.js', exports.default)
+    watch('src/**/*.js', exports.default)
 }
